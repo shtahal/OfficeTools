bae9ec4f2e65938e989c56ab7c3b409f84e729e4
shtahal <phshlcmhcscl159>
2022-08-14 17:47:29 +0800

    添加项目文件。

---
+++ OfficeTools/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace OfficeTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectMinu = new System.Windows.Forms.NumericUpDown();
+            this.selectHour = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labelSec = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelMinu2 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelHour2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelMinu = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelHour = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.检查更新ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.selectMinu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectHour)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.selectMinu);
+            this.groupBox1.Controls.Add(this.selectHour);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "下班时间";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(159, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "分";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(80, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "时";
+            // 
+            // selectMinu
+            // 
+            this.selectMinu.Location = new System.Drawing.Point(109, 33);
+            this.selectMinu.Name = "selectMinu";
+            this.selectMinu.Size = new System.Drawing.Size(44, 24);
+            this.selectMinu.TabIndex = 1;
+            this.selectMinu.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // selectHour
+            // 
+            this.selectHour.Location = new System.Drawing.Point(31, 33);
+            this.selectHour.Name = "selectHour";
+            this.selectHour.Size = new System.Drawing.Size(43, 24);
+            this.selectHour.TabIndex = 0;
+            this.selectHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.labelSec);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.labelMinu2);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.labelHour2);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.labelMinu);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.labelHour);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(232, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(270, 121);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "下班提醒";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("宋体", 12F);
+            this.label13.Location = new System.Drawing.Point(236, 78);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(23, 16);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "秒";
+            // 
+            // labelSec
+            // 
+            this.labelSec.AutoSize = true;
+            this.labelSec.Font = new System.Drawing.Font("宋体", 15F);
+            this.labelSec.ForeColor = System.Drawing.Color.Red;
+            this.labelSec.Location = new System.Drawing.Point(210, 75);
+            this.labelSec.Name = "labelSec";
+            this.labelSec.Size = new System.Drawing.Size(19, 20);
+            this.labelSec.TabIndex = 13;
+            this.labelSec.Text = "0";
+            this.labelSec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 12F);
+            this.label9.Location = new System.Drawing.Point(183, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 16);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "分";
+            // 
+            // labelMinu2
+            // 
+            this.labelMinu2.AutoSize = true;
+            this.labelMinu2.Font = new System.Drawing.Font("宋体", 15F);
+            this.labelMinu2.ForeColor = System.Drawing.Color.Red;
+            this.labelMinu2.Location = new System.Drawing.Point(158, 75);
+            this.labelMinu2.Name = "labelMinu2";
+            this.labelMinu2.Size = new System.Drawing.Size(19, 20);
+            this.labelMinu2.TabIndex = 11;
+            this.labelMinu2.Text = "0";
+            this.labelMinu2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("宋体", 12F);
+            this.label11.Location = new System.Drawing.Point(129, 78);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(23, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "时";
+            // 
+            // labelHour2
+            // 
+            this.labelHour2.AutoSize = true;
+            this.labelHour2.Font = new System.Drawing.Font("宋体", 15F);
+            this.labelHour2.ForeColor = System.Drawing.Color.Red;
+            this.labelHour2.Location = new System.Drawing.Point(104, 75);
+            this.labelHour2.Name = "labelHour2";
+            this.labelHour2.Size = new System.Drawing.Size(19, 20);
+            this.labelHour2.TabIndex = 9;
+            this.labelHour2.Text = "0";
+            this.labelHour2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 12F);
+            this.label8.Location = new System.Drawing.Point(183, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "分";
+            // 
+            // labelMinu
+            // 
+            this.labelMinu.AutoSize = true;
+            this.labelMinu.Font = new System.Drawing.Font("宋体", 15F);
+            this.labelMinu.ForeColor = System.Drawing.Color.Red;
+            this.labelMinu.Location = new System.Drawing.Point(158, 31);
+            this.labelMinu.Name = "labelMinu";
+            this.labelMinu.Size = new System.Drawing.Size(19, 20);
+            this.labelMinu.TabIndex = 7;
+            this.labelMinu.Text = "0";
+            this.labelMinu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 12F);
+            this.label6.Location = new System.Drawing.Point(129, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "时";
+            // 
+            // labelHour
+            // 
+            this.labelHour.AutoSize = true;
+            this.labelHour.Font = new System.Drawing.Font("宋体", 15F);
+            this.labelHour.ForeColor = System.Drawing.Color.Red;
+            this.labelHour.Location = new System.Drawing.Point(104, 31);
+            this.labelHour.Name = "labelHour";
+            this.labelHour.Size = new System.Drawing.Size(19, 20);
+            this.labelHour.TabIndex = 2;
+            this.labelHour.Text = "0";
+            this.labelHour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "下班倒计时：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "下班时间：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "OfficeTool(v1.0)";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "v1.0 By Echo";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.检查更新ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(509, 25);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 检查更新ToolStripMenuItem
+            // 
+            this.检查更新ToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.检查更新ToolStripMenuItem.Name = "检查更新ToolStripMenuItem";
+            this.检查更新ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.检查更新ToolStripMenuItem.Text = "检查更新";
+            this.检查更新ToolStripMenuItem.Click += new System.EventHandler(this.Click_Update);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 211);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "下班提醒工具";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.selectMinu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectHour)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown selectMinu;
+        private System.Windows.Forms.NumericUpDown selectHour;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelHour;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelMinu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labelSec;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelMinu2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelHour2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 检查更新ToolStripMenuItem;
+    }
+}
+
---
+++ OfficeTools/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OfficeTools
+{
+    public partial class Form1 : Form
+    {
+        private bool tomorrowFlag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.selectHour.Value = DateTime.Now.Hour;
+            this.selectMinu.Value = DateTime.Now.Minute;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String hour = this.selectHour.Value.ToString(), minute = this.selectMinu.Value.ToString();
+
+            if (DateTime.Now.Hour < Convert.ToInt32(hour))
+            {
+                this.confirmOk();
+                this.timer1.Start();
+            }
+            else if (DateTime.Now.Hour == Convert.ToInt32(hour))
+            {
+                if (DateTime.Now.Minute < Convert.ToInt32(minute))
+                {
+                    this.confirmOk();
+                    this.timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("下班时间需比当前时间晚！", "设置提醒", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.selectHour.Value = DateTime.Now.Hour;
+                    this.selectMinu.Value = DateTime.Now.Minute;
+                }
+            }
+            else // 第二天
+            {
+                tomorrowFlag = true;
+                //this.numericUpDown1.Value = DateTime.Now.Hour;
+                //this.numericUpDown2.Value = DateTime.Now.Minute;
+                this.confirmOk();
+                this.timer1.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("重新设置下班时间！", "设置提醒", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            this.timer1.Stop();
+            tomorrowFlag = false;
+
+            this.selectHour.Value = DateTime.Now.Hour;
+            this.selectMinu.Value = DateTime.Now.Minute;
+            this.selectHour.Enabled = true;
+            this.selectMinu.Enabled = true;
+
+            this.labelHour.Text = "0";
+            this.labelMinu.Text = "0";
+            this.labelHour2.Text = "0";
+            this.labelMinu2.Text = "0";
+            this.labelSec.Text = "0";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            String hour = this.selectHour.Value.ToString(), minute = this.selectMinu.Value.ToString();
+
+            String year = DateTime.Now.Year.ToString(), month = DateTime.Now.Month.ToString(), day = DateTime.Now.Day.ToString(); ;
+            if (tomorrowFlag)
+            {
+                day = DateTime.Now.AddDays(1).Day.ToString();
+            }
+
+            String planTimeStr1 = year + "/" + month + "/" + day;
+            String planTimeStr2 = hour + ":" + minute + ":00";
+            DateTime planTime = Convert.ToDateTime(planTimeStr1 + " " + planTimeStr2);
+
+            DateTime curTime = DateTime.Now;
+            TimeSpan ts = planTime - curTime;
+
+            if (ts.TotalMilliseconds > 0)
+            {
+                this.labelHour2.Text = ts.Hours.ToString();
+                this.labelMinu2.Text = ts.Minutes.ToString();
+                this.labelSec.Text = ts.Seconds.ToString();
+            }
+            else
+            {
+                this.timer1.Stop();
+                tomorrowFlag = false;
+                this.selectHour.Enabled = true;
+                this.selectMinu.Enabled = true;
+
+                //MessageBox.Show("下班了！下班了！下班了！", "设置提醒", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                using (var soundPlayer = new SoundPlayer(Environment.CurrentDirectory + "\\zhangyuge.wav"))
+                {
+                    soundPlayer.Play();
+                }
+
+                this.Show();
+                this.TopMost = true;
+                WindowState = FormWindowState.Normal;
+                this.Focus();
+
+                Form2 form2 = new Form2();
+                form2.Activate();
+                form2.Visible = true;
+                form2.TopMost = true;
+            }
+        }
+
+        private void confirmOk()
+        {
+            this.labelHour.Text = this.selectHour.Value.ToString();
+            this.labelMinu.Text = this.selectMinu.Value.ToString();
+            this.selectHour.Enabled = false;
+            this.selectMinu.Enabled = false;
+            MessageBox.Show("下班时间已设置！", "设置提醒", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.notifyIcon.Visible = false;
+            this.Show();
+            this.TopMost = true;
+            WindowState = FormWindowState.Normal;
+            this.Focus();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //this.ShowInTaskbar = false;
+                this.Visible = false;
+                this.notifyIcon.Visible = true;
+                this.notifyIcon.ShowBalloonTip(2, "提示", "点击图标恢复", ToolTipIcon.Info);
+            }
+            
+        }
+
+        private void Click_Update(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("");
+        }
+    }
+}
